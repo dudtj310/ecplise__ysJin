62f892e92fea44e1d70e9d73639b4faefb26263e
dudtj310 <73187874+dudtj310@users.noreply.github.com>
2020-11-18 00:50:59 +0900

    PushPrint.java modifed on repository

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -3,7 +3,7 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("Sophomore");
-		System.out.println("Wankwang Univ.");
+		System.out.println("Jeonbuk Iskan");
 	}
 
 }
